0d6621694f26b3b848fe432b65d09a54e6c127c4
Justin Tackett <justin.carl.tackett@gmail.com>
2022-02-24 11:51:32 -0700

    Unstable Ksat

--- ksat.jl
+++ ksat.jl
@@ -21,36 +21,41 @@ function reduceC(varNext::Int, status::Bool)
 
         # Going through the clauses and updating the statuses with new varNext status
         for i in 1:length(clauseList)
-                #checkForRedundancy()
-                if i > length(clauseList)
-                        break
-                end
-                if clauseStatus[clauseList[i]] == true # If clause status is true, we're done with this one
-                        nothing
-                else
-                        for j=1:length(clauseList[i]) # Checking each element of clause
+                try
+                        #checkForRedundancy()
+                        #if i > length(clauseList)
+                        #        break
+                        #end
+                        if clauseStatus[clauseList[i]] == true # If clause status is true, we're done with this one
+                                nothing
+                        else
+                                for j=1:length(clauseList[i]) # Checking each element of clause
 
-                                if j>length(clauseList[i]) # Catching error that arises when a variable is deleted
-                                        break
-                                end
-                                if varStatus[clauseList[i][j]] == true # If true, mark down for unwind and set clause to true
-                                        global clauseStatus[clauseList[i]] = true;
-                                        push!(clauseSet, i)
-                                elseif varStatus[clauseList[i][j]] == false # If false, take it out of clauses to reduce it down
-                                        if length(clauseList[i]) > 1 # If this clause is about to be reduced to zero, we have failed this try
-                                                push!(varSet,[clauseList[i][j],i,j])
-                                                delete!(clauseStatus, clauseList[i])
-                                                deleteat!(clauseList[i],j)
-                                                global clauseStatus[clauseList[i]] = NaN
-                                                println("Post deleting status")
-                                                println(clauseStatus)
-                                                println("Post deleting clauseList[i]")
-                                                println(clauseList[i])
-                                        else
-                                                return false, varSet, clauseSet
+                                        if j>length(clauseList[i]) # Catching error that arises when a variable is deleted
+                                                break
+                                        end
+                                        if varStatus[clauseList[i][j]] == true # If true, mark down for unwind and set clause to true
+                                                global clauseStatus[clauseList[i]] = true;
+                                                push!(clauseSet, i)
+                                        elseif varStatus[clauseList[i][j]] == false # If false, take it out of clauses to reduce it down
+                                                if length(clauseList[i]) > 1 # If this clause is about to be reduced to zero, we have failed this try
+                                                        push!(varSet,[clauseList[i][j],i,j])
+                                                        delete!(clauseStatus, clauseList[i])
+                                                        deleteat!(clauseList[i],j)
+                                                        global clauseStatus[clauseList[i]] = NaN
+                                                        println("Post deleting status")
+                                                        println(clauseStatus)
+                                                        println("Post deleting clauseList[i]")
+                                                        println(clauseList[i])
+                                                else
+                                                        return false, varSet, clauseSet
+                                                end
                                         end
                                 end
                         end
+                catch
+                        println("ERROR")
+                        continue
                 end
         end
         return true, varSet, clauseSet
@@ -180,12 +185,12 @@ function solveT()
 
 
         varNext = i;
-        if typeof(checkForSinglets()) == Int64
-                varNext = checkForSinglets();
-        end
-        if typeof(varNext) == Bool # Failsafe in case somehow varNext becomes false
-                varNext = i
-        end
+        #if typeof(checkForSinglets()) == Int64
+        #        varNext = checkForSinglets();
+        #end
+        #if typeof(varNext) == Bool # Failsafe in case somehow varNext becomes false
+        #        varNext = i
+        #end
 
 
         for m in [true, false]
